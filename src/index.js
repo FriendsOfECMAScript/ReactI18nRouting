b3469fbf2f018a4d34713b3920c138be66a47a97
Mikel Tuesta <mikeltuesta@gmail.com>
2018-06-19 12:07:43 +0200

    Added a FormattedNavLink implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import BrowserIntlProvider from './component/BrowserIntlProvider.js';
 import FormattedLink from './component/FormattedLink.js';
+import FormattedNavLink from './component/FormattedNavLink.js';
 import defaultUnPrefixed from './languageStrategy/defaultUnprefixed.js';
 import subdomainBased from './languageStrategy/subdomainBased.js';
 import * as Reducer from './reducer.js';
@@ -8,6 +9,7 @@ import * as RenderTranslatedRoutes from './renderTranslatedRoutes.js';
 export {
   BrowserIntlProvider,
   FormattedLink,
+  FormattedNavLink,
   defaultUnPrefixed,
   subdomainBased,
   Reducer,
