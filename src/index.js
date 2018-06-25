5096aeaf7018204b33137f8b6d27e8298fb75557
benatespina <benatespina@gmail.com>
2018-06-25 19:23:58 +0200

    Added header licenses

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the ReactI18nRouting package.
+ *
+ * Copyright (c) 2018-present Friends Of ECMAScript
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import BrowserIntlProvider from './component/BrowserIntlProvider.js';
 import InMemoryBrowserIntlProvider from './component/bridges/InMemoryBrowserIntlProvider.js';
 import LocalStorageBrowserIntlProvider from './component/bridges/LocalStorageBrowserIntlProvider.js';
--- src/languageStrategy/pathFromRouteForPathsAndLocale.js
+++ src/languageStrategy/pathFromRouteForPathsAndLocale.js
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the ReactI18nRouting package.
+ *
+ * Copyright (c) 2018-present Friends Of ECMAScript
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 export default (defaultLocale, pathFromRoute) => (
   paths,
   locale,
