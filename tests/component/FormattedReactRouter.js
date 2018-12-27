8a3e10f2726c0555902f7147460de24997f6d825
Beñat Espiña <benatespina@gmail.com>
2018-12-27 15:57:06 +0100

    Added missing header licenses

--- tests/component/FormattedReactRouter.js
+++ tests/component/FormattedReactRouter.js
@@ -1,11 +1,20 @@
+/*
+ * This file is part of the ReactI18nRouting package.
+ *
+ * Copyright (c) 2018-present Friends Of ECMAScript
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import React from 'react';
 import {render} from 'react-testing-library';
 import createHistory from 'history/createMemoryHistory';
 import {Router} from 'react-router-dom';
 
-import I18nRoutingProvider from '../../src/component/I18nRoutingProvider';
-import defaultUnprefixed from '../../src/languageStrategy/defaultUnprefixed';
-import {FormattedLink, FormattedNavLink} from '../../src/component/FormattedReactRouter';
+import I18nRoutingProvider from './../../src/component/I18nRoutingProvider';
+import defaultUnprefixed from './../../src/languageStrategy/defaultUnprefixed';
+import {FormattedLink, FormattedNavLink} from './../../src/component/FormattedReactRouter';
 
 const languageStrategy = defaultUnprefixed({
   routes: {
--- tests/component/I18nRoutingProvider.js
+++ tests/component/I18nRoutingProvider.js
@@ -1,10 +1,19 @@
+/*
+ * This file is part of the ReactI18nRouting package.
+ *
+ * Copyright (c) 2018-present Friends Of ECMAScript
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import React from 'react';
 import {render} from 'react-testing-library';
 import createHistory from 'history/createMemoryHistory';
 
-import I18nRoutingProvider from '../../src/component/I18nRoutingProvider';
-import withI18nRouting from '../../src/component/withI18nRouting';
-import defaultUnprefixed from '../../src/languageStrategy/defaultUnprefixed';
+import I18nRoutingProvider from './../../src/component/I18nRoutingProvider';
+import withI18nRouting from './../../src/component/withI18nRouting';
+import defaultUnprefixed from './../../src/languageStrategy/defaultUnprefixed';
 
 const languageStrategy = defaultUnprefixed({
   routes: [],
