808f266e8e8b95db9740460aa14366833a7cdb07
Beñat Espiña <benatespina@gmail.com>
2018-12-27 15:57:45 +0100

    Added missing header license

--- src/component/LocaleContext.js
+++ src/component/LocaleContext.js
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the ReactI18nRouting package.
+ *
+ * Copyright (c) 2018-present Friends Of ECMAScript
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import withI18nRouting from './withI18nRouting';
 
 export default withI18nRouting(({i18nRouting, children}) => children(i18nRouting.locale));
